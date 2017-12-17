acf1a09172e01ef041d6a03f3db2f7559625e202
Matthieu Vergne <matthieu.vergne@gmail.com>
2017-12-17 22:56:01 +0100

    WIP

--- jmetal-core/src/main/java/org/uma/jmetal/util/solutionattribute/impl/DominanceRanking.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/solutionattribute/impl/DominanceRanking.java
@@ -6,29 +6,29 @@ import org.uma.jmetal.util.comparator.DominanceComparator;
 import org.uma.jmetal.util.comparator.impl.OverallConstraintViolationComparator;
 import org.uma.jmetal.util.solutionattribute.Ranking;
 
+import java.io.PrintStream;
 import java.util.*;
 
 /**
- * This class implements some facilities for ranking set of solutions.
- * Given a collection of solutions, they are ranked
- * according to scheme proposed in NSGA-II; as an output, a set of subsets
- * are obtained. The subsets are numbered starting from 0 (in NSGA-II, the
- * numbering starts from 1); thus, subset 0 contains the non-dominated
- * solutions, subset 1 contains the non-dominated solutions after removing those
- * belonging to subset 0, and so on.
+ * This class implements some facilities for ranking set of solutions. Given a
+ * collection of solutions, they are ranked according to scheme proposed in
+ * NSGA-II; as an output, a set of subsets are obtained. The subsets are
+ * numbered starting from 0 (in NSGA-II, the numbering starts from 1); thus,
+ * subset 0 contains the non-dominated solutions, subset 1 contains the
+ * non-dominated solutions after removing those belonging to subset 0, and so
+ * on.
  *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  * @author Juan J. Durillo
  */
 @SuppressWarnings("serial")
-public class DominanceRanking <S extends Solution<?>>
-    extends GenericSolutionAttribute<S, Integer> implements Ranking<S> {
+public class DominanceRanking<S extends Solution<?>> extends GenericSolutionAttribute<S, Integer>
+    implements Ranking<S> {
 
   private static final Comparator<Solution<?>> DOMINANCE_COMPARATOR = new DominanceComparator<Solution<?>>();
-  private static final Comparator<Solution<?>> CONSTRAINT_VIOLATION_COMPARATOR =
-      new OverallConstraintViolationComparator<Solution<?>>();
+  private static final Comparator<Solution<?>> CONSTRAINT_VIOLATION_COMPARATOR = new OverallConstraintViolationComparator<Solution<?>>();
 
-  private List<ArrayList<S>> rankedSubPopulations;
+  private List<List<S>> rankedSubPopulations;
 
   /**
    * Constructor
@@ -38,100 +38,80 @@ public class DominanceRanking <S extends Solution<?>>
   }
 
   public DominanceRanking(Object id) {
-    super(id) ;
+    super(id);
     rankedSubPopulations = new ArrayList<>();
   }
 
   @Override
   public Ranking<S> computeRanking(List<S> solutionSet) {
-    List<S> population = solutionSet;
-
-    // dominateMe[i] contains the number of solutions dominating i
-    int[] dominateMe = new int[population.size()];
-
-    // iDominate[k] contains the list of solutions dominated by k
-    List<List<Integer>> iDominate = new ArrayList<>(population.size());
-
-    // front[i] contains the list of individuals belonging to the front i
-    ArrayList<List<Integer>> front = new ArrayList<>(population.size() + 1);
-
-    // Initialize the fronts
-    for (int i = 0; i < population.size() + 1; i++) {
-      front.add(new LinkedList<Integer>());
-    }
-
-    // Fast non dominated sorting algorithm
-    // Contribution of Guillaume Jacquenot
-    for (int p = 0; p < population.size(); p++) {
-      // Initialize the list of individuals that i dominate and the number
-      // of individuals that dominate me
-      iDominate.add(new LinkedList<Integer>());
-      dominateMe[p] = 0;
-    }
-
-    int flagDominate;
-    for (int p = 0; p < (population.size() - 1); p++) {
-      // For all q individuals , calculate if p dominates q or vice versa
-      for (int q = p + 1; q < population.size(); q++) {
-        flagDominate =
-            CONSTRAINT_VIOLATION_COMPARATOR.compare(solutionSet.get(p), solutionSet.get(q));
+    if (solutionSet.isEmpty()) {
+      rankedSubPopulations = new LinkedList<>();
+    } else {
+      Comparator<S> fullComparator = (s1, s2) -> {
+        int flagDominate = CONSTRAINT_VIOLATION_COMPARATOR.compare(s1, s2);
         if (flagDominate == 0) {
-          flagDominate = DOMINANCE_COMPARATOR.compare(solutionSet.get(p), solutionSet.get(q));
+          return DOMINANCE_COMPARATOR.compare(s1, s2);
+        } else {
+          return flagDominate;
         }
-        if (flagDominate == -1) {
-          iDominate.get(p).add(q);
-          dominateMe[q]++;
-        } else if (flagDominate == 1) {
-          iDominate.get(q).add(p);
-          dominateMe[p]++;
+      };
+
+      Map<S, Integer> arbitrarySort = new HashMap<>(solutionSet.size());
+      solutionSet.forEach(s -> arbitrarySort.put(s, arbitrarySort.size()));
+      Comparator<S> totalComparator = (s1, s2) -> {
+        int flagDominate = fullComparator.compare(s1, s2);
+        if (flagDominate == 0) {
+          flagDominate = Integer.compare(arbitrarySort.get(s1), arbitrarySort.get(s2));
         }
-      }
-    }
+        String operator = flagDominate == 1 ? ">" : flagDominate == -1 ? "<" : "=";
+        PrintStream writer = (PrintStream) s1.getAttribute("writer");
+        writer.println("REL:"+s1.getAttribute("name") + operator + s2.getAttribute("name"));
+        return flagDominate;
+      };
 
-    for (int i = 0; i < population.size(); i++) {
-      if (dominateMe[i] == 0) {
-        front.get(0).add(i);
-        solutionSet.get(i).setAttribute(getAttributeIdentifier(), 0);
-      }
-    }
+      List<S> sortedPopulation = new LinkedList<>(solutionSet);// TODO ArrayList?
+      sortedPopulation.sort(totalComparator);
+      List<List<S>> fronts = splitIntoFronts(sortedPopulation, fullComparator);
+      setSolutionAttributes(fronts);
 
-    //Obtain the rest of fronts
-    int i = 0;
-    Iterator<Integer> it1, it2; // Iterators
-    while (front.get(i).size() != 0) {
-      i++;
-      it1 = front.get(i - 1).iterator();
-      while (it1.hasNext()) {
-        it2 = iDominate.get(it1.next()).iterator();
-        while (it2.hasNext()) {
-          int index = it2.next();
-          dominateMe[index]--;
-          if (dominateMe[index] == 0) {
-            front.get(i).add(index);
-            //RankingAndCrowdingAttr.getAttributes(solutionSet.get(index)).setRank(i);
-            solutionSet.get(index).setAttribute(getAttributeIdentifier(), i);
-          }
-        }
-      }
-    }
-
-    rankedSubPopulations = new ArrayList<>();
-    //0,1,2,....,i-1 are fronts, then i fronts
-    for (int j = 0; j < i; j++) {
-      rankedSubPopulations.add(j, new ArrayList<S>(front.get(j).size()));
-      it1 = front.get(j).iterator();
-      while (it1.hasNext()) {
-        rankedSubPopulations.get(j).add(solutionSet.get(it1.next()));
-      }
+      rankedSubPopulations = fronts;
     }
 
     return this;
   }
 
+  private List<List<S>> splitIntoFronts(List<S> sortedPopulation, Comparator<S> fullComparator) {
+    List<List<S>> fronts = new LinkedList<>();
+    int referenceIndex = 0;
+    for (int solutionIndex = 0; solutionIndex < sortedPopulation.size(); solutionIndex++) {
+      S ref = sortedPopulation.get(referenceIndex);
+      S sol = sortedPopulation.get(solutionIndex);
+      if (fullComparator.compare(ref, sol) == 0) {
+        // Still in the same front
+      } else {
+        List<S> front = sortedPopulation.subList(referenceIndex, solutionIndex);
+        fronts.add(front);
+        referenceIndex = solutionIndex;
+      }
+    }
+    fronts.add(sortedPopulation.subList(referenceIndex, sortedPopulation.size()));
+    return fronts;
+  }
+
+  private void setSolutionAttributes(List<List<S>> fronts) {
+    Object id = getAttributeIdentifier();
+    for (int frontIndex = 0; frontIndex < fronts.size(); frontIndex++) {
+      List<S> front = fronts.get(frontIndex);
+      for (S solution : front) {
+        solution.setAttribute(id, frontIndex);
+      }
+    }
+  }
+
   @Override
   public List<S> getSubfront(int rank) {
     if (rank >= rankedSubPopulations.size()) {
-      throw new JMetalException("Invalid rank: " + rank + ". Max rank = " + (rankedSubPopulations.size() -1)) ;
+      throw new JMetalException("Invalid rank: " + rank + ". Max rank = " + (rankedSubPopulations.size() - 1));
     }
     return rankedSubPopulations.get(rank);
   }
