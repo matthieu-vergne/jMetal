0349cdd23693c15111b0557644b624f47d1ceb1a
Matthieu Vergne <matthieu.vergne@gmail.com>
2019-03-30 14:47:54 +0100

    Fix: typo.

--- jmetal-service/src/main/java/org/uma/jmetal/service/model/runnable/ParamsExample.java
+++ jmetal-service/src/main/java/org/uma/jmetal/service/model/runnable/ParamsExample.java
@@ -7,12 +7,12 @@ public class ParamsExample extends ResourceSupport {
 
 	public final String description;
 
-	public ParamsExample(ResourceSupport prent, String parentId, String parentRel) {
+	public ParamsExample(ResourceSupport parent, String parentId, String parentRel) {
 		this.description = "Here should appear an example of parameters for " + parentId
 				+ ", based on their definition, and ready for copy-paste-run.";
-		add(prent.getLink(Link.REL_PARAMS_EXAMPLE).withRel(Link.REL_SELF));
-		add(prent.getLink(Link.REL_SELF).withRel(parentRel));
-		add(prent.getLink(Link.REL_PARAMS_DEFINITION));
+		add(parent.getLink(Link.REL_PARAMS_EXAMPLE).withRel(Link.REL_SELF));
+		add(parent.getLink(Link.REL_SELF).withRel(parentRel));
+		add(parent.getLink(Link.REL_PARAMS_DEFINITION));
 	}
 
 }
